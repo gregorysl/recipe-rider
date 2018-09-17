b956f637ab584b381c05577b7236af6d1a9c628b
Grzegorz Slazak <grzegorz.slazak@fenergo.com>
2018-09-17 14:36:59 +0200

    enable redux dev tools

--- store/index.js
+++ store/index.js
@@ -1,17 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers/reducers';
 import sagas from '../sagas/sagas';
 
 const configureStore = () => {
   const sagaMiddleware = createSagaMiddleware();
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return {
     ...createStore(
       rootReducer,
-      applyMiddleware(sagaMiddleware),
+      composeEnhancers(applyMiddleware(sagaMiddleware))
     ),
     runSaga: sagaMiddleware.run
   };
+  /* eslint-enable */
 };
 
 const store = configureStore();
