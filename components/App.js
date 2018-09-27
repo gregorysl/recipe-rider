3af03e19c127417674bea1d3db9fdabcba4efc20
Unknown <gregorysl@gmail.com>
2018-09-27 22:13:52 +0200

    fix proptypes

--- components/App.js
+++ components/App.js
@@ -27,8 +27,8 @@ class App extends Component {
   render() {
     const data = this.props.table.map(x => (<h2 key={x.key}>{x.name}</h2>));
     const data2 = this.props.products.map(x => (
-      <h3>
-        {Object.entries(x).map(z => (`${z[0]}:${z[1]}, `))}
+      <h3 key={x.key}>
+        {Object.entries(x).map(z => (<span key={z[0]}>{z[0]}:{z[1]}, </span>))}
         <Button onClick={() => this.handleClick(x)}>Edit</Button>
       </h3>));
     return (
@@ -53,8 +53,21 @@ class App extends Component {
 }
 
 App.propTypes = {
-  table: PropTypes.arrayOf(PropTypes.string).isRequired,
-  products: PropTypes.arrayOf(PropTypes.object).isRequired,
+  products: PropTypes.arrayOf(PropTypes.shape({
+    active: PropTypes.bool.isRequired,
+    bigSpoon: PropTypes.number.isRequired,
+    glass: PropTypes.number.isRequired,
+    grams: PropTypes.number.isRequired,
+    key: PropTypes.number.isRequired,
+    measurement: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    smallSpoon: PropTypes.number.isRequired,
+    unitPrice: PropTypes.number.isRequired
+  }).isRequired).isRequired,
+  table: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired
+  }).isRequired).isRequired,
   getRecipes: PropTypes.func.isRequired,
   getProducts: PropTypes.func.isRequired
 };
