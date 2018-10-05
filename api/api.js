5a2a30794d40bc2b25999fc5125e2b1941f0e1af
Grzegorz Slazak <gregorysl@gmail.com>
2018-10-06 00:24:18 +0200

    add measurement types

--- api/api.js
+++ api/api.js
@@ -1,28 +1,36 @@
-const recipes = [{ name: 'asd', key: 1 }];
+const recipes = [
+  {
+    key: 1,
+    name: 'Jajecznik',
+    products: [
+      { amount: '3', product: '1', measurement: 'glass' },
+      { amount: '100', product: '2', measurement: 'grams' }
+    ]
+  }
+];
 let currentProductKey = 3;
 let currentRecipeKey = 2;
-const products = [{
-  key: 1,
-  bigSpoon: 123,
-  glass: 234,
-  grams: 345,
-  name: 'Jajka',
-  measurement: 'glass',
-  smallSpoon: 456,
-  unitPrice: 567,
-  active: true
-},
-{
-  key: 2,
-  bigSpoon: 123,
-  glass: 123,
-  grams: 123,
-  name: 'Mąka',
-  measurement: 'grams',
-  smallSpoon: 123,
-  unitPrice: 123,
-  active: false
-}];
+const products = [
+  {
+    key: 1,
+    unitPrice: 4.2,
+    name: 'Jajka',
+    measurement: 'piece',
+    piece: 10,
+    active: true
+  },
+  {
+    key: 2,
+    bigSpoon: 123,
+    glass: 123,
+    grams: 123,
+    name: 'Mąka',
+    measurement: 'grams',
+    smallSpoon: 123,
+    unitPrice: 123,
+    active: false
+  }
+];
 
 export function addRecipe(recipe) {
   recipe.key = currentRecipeKey;
@@ -49,3 +57,31 @@ export function saveProduct(product) {
 export function getProducts() {
   return products;
 }
+
+export const measurementTypes = [
+  {
+    key: 'piece',
+    name: 'Sztuka',
+    main: true
+  },
+  {
+    key: 'grams',
+    name: 'Gramy',
+    main: true
+  },
+  {
+    key: 'glass',
+    name: 'Szklanka',
+    parent: 'grams'
+  },
+  {
+    key: 'bigSpoon',
+    name: 'Duża łyżka',
+    parent: 'grams'
+  },
+  {
+    key: 'smallSpoon',
+    name: 'Mała łyżeczka',
+    parent: 'grams'
+  }
+];
