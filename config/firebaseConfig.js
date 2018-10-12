bca231c6016c9cd54d44c2a266fc656ff369d368
Grzegorz Slazak <gregorysl@gmail.com>
2018-10-13 00:04:32 +0200

    initial firebase commit

---
+++ config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: YOUR_API_KEY,
+  authDomain: YOUR_AUTH_DOMAIN,
+  databaseURL: YOUR_DATABASE_URL,
+  projectId: YOUR_PROJECT_ID,
+  storageBucket: '',
+  messagingSenderId: YOUR_MESSAGING_SENDER_ID
+};
+
+export default config;
